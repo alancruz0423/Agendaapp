0fb85e316770af404c39d74f2a4b0307d57a4f37
Alan <tu_correo@ejemplo.com>
2026-05-26 09:16:25 -0600

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  }
+}
